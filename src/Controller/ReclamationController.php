11ae1108eeffae3579413bcc17d0aeb37a47862a
Riadh <Riadhrabti@gmail.com>
2022-05-09 22:42:28 +0200

    commit 9

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -41,7 +41,7 @@ class ReclamationController extends AbstractController
         if ($etat == -1) {
             $reclamations = $repository->findAll();
         } else {
-            $reclamations = $repository->findBy(['etat'=>$etat]);
+            $reclamations = $repository->findBy(['etat' => $etat]);
         }
 
         return $this->render('Reclamation/ListReclamation.html.twig', [
@@ -49,77 +49,81 @@ class ReclamationController extends AbstractController
             'Reclamation' => $reclamations
         ]);
     }
+
     /**
      * @Route("/DeleteReclamation/{id}",name="delete")
      */
-    public  function  deleteReclamtion($id){
-        $em=$this->getDoctrine()->getManager();
-        $Reclamation=$em->getRepository(Reclamation::class)->find($id);
-        $em->remove( $Reclamation);
+    public function deleteReclamtion($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $Reclamation = $em->getRepository(Reclamation::class)->find($id);
+        $em->remove($Reclamation);
         $em->flush();
         return $this->redirectToRoute('Reclamations');
 
     }
+
     /**
-     * @param  Request $request
-     *@return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      * @Route ("/addReclamation/{id}" , name="addReclamation");
      */
-    function addReclamation(Request $request,$id,FlashyNotifier $flashy)
+    function addReclamation(Request $request, $id, FlashyNotifier $flashy)
     {
 
-        $Reclamation =new Reclamation();
+        $Reclamation = new Reclamation();
         $Reclamation->setIdechange($id);
         $Reclamation->setEtat(0);
         $Reclamation->setDaterec(new \DateTime());
 
-        $form=$this->createForm(ReclamationAddType::class,$Reclamation);
+        $form = $this->createForm(ReclamationAddType::class, $Reclamation);
 
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $em=$this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->persist($Reclamation);
             $em->flush();
             $flashy->success('Reclamation envoyée');
-            return  $this->redirectToRoute('Reclamations');
+            return $this->redirectToRoute('Reclamations');
         }
 
-        return $this->render('Reclamation/addReclamation.html.twig',[
+        return $this->render('Reclamation/addReclamation.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
     /**
-     * @param  Request $request
-     *@return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      * @Route ("Reclamation/update/{id}",name="update");
      */
-    function  updateReclamation(Request $request ,ReclamationRepository $repository ,$id)
+    function updateReclamation(Request $request, ReclamationRepository $repository, $id)
     {
 
-        $Reclamation=$repository->find($id);
-        $form=$this->createForm(ReclamationAddType::class,$Reclamation);
+        $Reclamation = $repository->find($id);
+        $form = $this->createForm(ReclamationAddType::class, $Reclamation);
         $form->add('update', SubmitType::class);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $em=$this->getDoctrine()->getManager();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
             $em->flush();
-            return  $this->redirectToRoute('Reclamations');
+            return $this->redirectToRoute('Reclamations');
         }
 
-        return $this->render('Reclamation/update.html.twig',[
+        return $this->render('Reclamation/update.html.twig', [
             'form' => $form->createView()
 
         ]);
 
     }
+
     /**
-     * @param  Request $request
-     *@return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      * @Route ("Reclamation/accepter/{id}",name="accepter");
      */
-    function  accpeterReclamation(Request $request ,ReclamationRepository $repository ,$id,FlashyNotifier $flashy,\Swift_Mailer $mailer)
+    function accpeterReclamation(Request $request, ReclamationRepository $repository, $id, FlashyNotifier $flashy, \Swift_Mailer $mailer)
     {
 
         $Reclamation = $repository->find($id);
@@ -128,24 +132,24 @@ class ReclamationController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $em->flush();
         $message = (new \Swift_Message('You Got Mail!'))
-                ->setFrom('riadhrabti@gmail.com')
-                ->setTo('riadh.rabti@esprit.tn')
-                ->setBody(
-                    'votre reclamation a été accepté par l admin du page '
-        )
-    ;
+            ->setFrom('riadhrabti@gmail.com')
+            ->setTo('riadh.rabti@esprit.tn')
+            ->setBody(
+                'votre reclamation a été accepté par l admin du page '
+            );
 
-           $mailer->send($message);
+        $mailer->send($message);
         $flashy->info('Reclamation accepté');
         return $this->redirectToRoute('Reclamations');
 
     }
+
     /**
-     * @param  Request $request
-     *@return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      * @Route ("Reclamation/refuser/{id}",name="refuser");
      */
-    function  refuserReclamation(Request $request ,ReclamationRepository $repository ,$id,FlashyNotifier $flashy,\Swift_Mailer $mailer)
+    function refuserReclamation(Request $request, ReclamationRepository $repository, $id, FlashyNotifier $flashy, \Swift_Mailer $mailer)
     {
 
         $Reclamation = $repository->find($id);
@@ -158,8 +162,7 @@ class ReclamationController extends AbstractController
             ->setTo('riadh.rabti@esprit.tn')
             ->setBody(
                 'votre reclamation a été refusé par l admin du page '
-            )
-        ;
+            );
 
         $mailer->send($message);
         $flashy->warning('Reclamation refusé');
@@ -171,11 +174,15 @@ class ReclamationController extends AbstractController
     /**
      * @Route("/checkReclamation",name="checkReclamation")
      */
-    public  function  checkReclamation(Request $request, ReclamationRepository $reclamationRepository){
+    public function checkReclamation(Request $request, ReclamationRepository $reclamationRepository)
+    {
 
         if ($request->isXMLHttpRequest()) {
             $idEchange = intval($request->get('idEchange'));
-            $reclamations = $reclamationRepository->findBy(['idechange'=>$idEchange], ['Daterec'=>'DESC']);
+            $reclamations = $reclamationRepository->findBy(['idechange' => $idEchange], ['Daterec' => 'DESC']);
+            if ($reclamations == null) {
+                return new JsonResponse(array('data' => true));
+            }
             $lastReclamationDate = $reclamations[0]->getDaterec();
             $date = new \DateTime();
             $deffirence = $lastReclamationDate->diff($date)->d;
